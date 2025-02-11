ed6fd5066dd92ba1850f4960e9927e36f7498c30
lupo90 <lucadileonardo90@gmail.com>
2025-02-11 17:49:41 +0100

    aggiornamento calcite

--- map_landslide/main.js
+++ map_landslide/main.js
@@ -2,14 +2,158 @@ var map = new ol.Map({
     target: 'map',
     layers: [
         new ol.layer.Tile({
-            source: new ol.source.OSM()
+            source: new ol.source.XYZ({
+                url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+            })
         })
+        
     ],
     view: new ol.View({
         center: ol.proj.fromLonLat([13.8333, 42.2167]),
-        zoom: 10
+        zoom: 9
     })
 });
+
+
+fetch('json/aree_poly_abruzzo_opendata.json')
+    .then(response => response.json())
+    .then(data => {
+        this.filterData_4 = data.features.filter(f => f.properties.nome_tipo === 'Aree con sprofondamenti diffusi')
+        this.filterData_5 = data.features.filter(f => f.properties.nome_tipo === 'Aree con frane superficiali diffuse')
+        this.filterData_6 = data.features.filter(f => f.properties.tipo_movimento === 'Aree con crolli/ribaltamenti diffusi')
+        
+
+
+        filterJson_5 = {
+            "type": "FeatureCollection",
+            "features": filterData_5,
+            "totalFeatures": 0,
+            "numberMatched": 0,
+            "numberReturned": 0,
+            "timeStamp": "2025-02-06T03:12:57.163Z",
+            "crs": {
+                "type": "name",
+                "properties": {
+                    "name": "urn:ogc:def:crs:EPSG::32632"
+                }
+            },
+            "bbox": [
+                840098.0432,
+                4631932.4413,
+                973824.8555,
+                4760013.1335
+            ]
+        }
+
+        VectorSource_5 = new ol.source.Vector({
+            features: new ol.format.GeoJSON().readFeatures(filterJson_5, {
+                featureProjection: 'EPSG:3857'
+            })
+        });
+
+
+        vectorLayer_5 = new ol.layer.Vector({
+            source: VectorSource_5,
+            style: new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'rgba(195, 215, 17, 0.66)',
+                    width: 1
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(185, 215, 17, 0.66)'
+                }),
+            })
+        });
+        map.addLayer(vectorLayer_5);
+
+
+        filterJson_4 = {
+            "type": "FeatureCollection",
+            "features": filterData_4,
+            "totalFeatures": 0,
+            "numberMatched": 0,
+            "numberReturned": 0,
+            "timeStamp": "2025-02-06T03:12:57.163Z",
+            "crs": {
+                "type": "name",
+                "properties": {
+                    "name": "urn:ogc:def:crs:EPSG::32632"
+                }
+            },
+            "bbox": [
+                840098.0432,
+                4631932.4413,
+                973824.8555,
+                4760013.1335
+            ]
+        }
+
+        VectorSource_4 = new ol.source.Vector({
+            features: new ol.format.GeoJSON().readFeatures(filterJson_4, {
+                featureProjection: 'EPSG:3857'
+            })
+        });
+
+
+        vectorLayer_4= new ol.layer.Vector({
+            source: VectorSource_4,
+            style: new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'rgba(17, 175, 215, 0.66)',
+                    width: 1
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(17, 175, 215, 0.66)'
+                }),
+            })
+        });
+        map.addLayer(vectorLayer_4);
+
+        filterJson_6 = {
+            "type": "FeatureCollection",
+            "features": filterData_6,
+            "totalFeatures": 0,
+            "numberMatched": 0,
+            "numberReturned": 0,
+            "timeStamp": "2025-02-06T03:12:57.163Z",
+            "crs": {
+                "type": "name",
+                "properties": {
+                    "name": "urn:ogc:def:crs:EPSG::32632"
+                }
+            },
+            "bbox": [
+                840098.0432,
+                4631932.4413,
+                973824.8555,
+                4760013.1335
+            ]
+        }
+
+        VectorSource_6 = new ol.source.Vector({
+            features: new ol.format.GeoJSON().readFeatures(filterJson_6, {
+                featureProjection: 'EPSG:3857'
+            })
+        });
+
+
+        vectorLayer_6 = new ol.layer.Vector({
+            source: VectorSource_6,
+            style: new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'rgba(17, 215, 106, 0.66)',
+                    width: 1
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(17, 215, 66, 0.66)'
+                }),
+            })
+        });
+        map.addLayer(vectorLayer_6);
+        console.log(data)
+    })
+    .catch(error => console.error('Errore nel caricamento del file JSON Aree:', error));
+
 
 
 fetch('json/frane_poly_abruzzo_opendata.json')
@@ -34,7 +178,7 @@ fetch('json/frane_poly_abruzzo_opendata.json')
         var filterData_2 = data.features.filter(f => f.properties.nome_tipo === 'Colamento lento')
         var filterData_3 = data.features.filter(f => f.properties.nome_tipo === 'Colamento rapido')
 
-
+        
         var filterJson = {
             "type": "FeatureCollection",
             "features": filterData,
@@ -274,42 +418,39 @@ fetch('json/frane_poly_abruzzo_opendata.json')
             })
         });
         map.addLayer(vectorLayer_3);
-
-
-        // Aggiunge evento click sulla mappa per selezionare una feature
-        // Cattura il click sulla mappa
-        map.on('click', function (event) {
-            let selectedFeature = null;
-
-            // Trova la feature cliccata
-            map.forEachFeatureAtPixel(event.pixel, function (feature) {
-                selectedFeature = feature; // Seleziona la feature cliccata
-                console.log(selectedFeature)
-
-                document.getElementById('widget_info').innerHTML = `<strong>Dati Frana</strong><br>
-                        <strong>ID Frana:</strong> ${feature.values_.id_frana}<br>
-                        
-                        <strong>Comune:</strong> ${feature.values_.nome_com} <br>
-
-                        <strong>Provincia:</strong> ${feature.values_.nome_prov}<br>
-
-                        <strong>Regione:</strong> ${feature.values_.nome_reg} <br>
-
-                        <strong>Distretto:</strong> ${feature.values_.nome_distr} <br>
-                        
-                        <strong>Movimento:</strong> ${feature.values_.nome_tipo}<br>`;
-            });
-        });
-
-
-
     })
-    .catch(error => console.error('Errore nel caricamento del file JSON:', error));
+    .catch(error => console.error('Errore nel caricamento del file JSON Frane:', error));
+
+
+
+    // Aggiunge evento click sulla mappa per selezionare una feature
+    map.on('click', function (event) {
+        let selectedFeature = null;
+
+        // Trova la feature cliccata
+        map.forEachFeatureAtPixel(event.pixel, function (feature) {
+            selectedFeature = feature; // Seleziona la feature cliccata
+            console.log(selectedFeature)
+
+            document.getElementById('widget_info').innerHTML = `<strong>Dati Frana</strong><br>
+                    <strong>ID Frana:</strong> ${feature.values_.id_frana}<br>
+                    
+                    <strong>Comune:</strong> ${feature.values_.nome_com} <br>
+
+                    <strong>Provincia:</strong> ${feature.values_.nome_prov}<br>
+
+                    <strong>Regione:</strong> ${feature.values_.nome_reg} <br>
+
+                    <strong>Distretto:</strong> ${feature.values_.nome_distr} <br>
+                    
+                    <strong>Movimento:</strong> ${feature.values_.nome_tipo}<br>`;
+        });
+    });
 
 
 // Funzione di ricerca via con Nominatim OpenStreetMap
-document.getElementById('search-button').addEventListener('click', function () {
-    let searchQuery = document.getElementById('search-input').value;
+document.getElementById('calcite-button').addEventListener('click', function () {
+    let searchQuery = document.getElementById('calcite-input').value;
 
     if (searchQuery) {
         fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${searchQuery}`)
